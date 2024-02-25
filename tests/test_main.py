60fe412d69f189f07a6d0f5b579c99d994390492
merry-mouse <potekh.anastasia@gmail.com>
2024-02-25 12:36:02 +0100

    Make second test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,3 @@
-import string
-
-import pytest
-
 from my_project.main import generate_random_string
 
 
@@ -10,3 +6,14 @@ def test_generate_random_string_length():
     length = 20
     result = generate_random_string(length)
     assert len(result) == length
+
+
+def test_generate_random_string_randomness():
+    # Checking if all generated strings are different
+    result1 = generate_random_string()
+    result2 = generate_random_string()
+    result3 = generate_random_string()
+    result4 = generate_random_string()
+    result5 = generate_random_string()
+    random_set = {result1, result2, result3, result4, result5}
+    assert len(random_set) == 5
